8b9cb057411331314516f55f1c2b65f8ae04828f
abhijeet nardele <234410808+abhijeetnardele24-hash@users.noreply.github.com>
2026-04-06 13:44:33 +0530

    docs: add interface usage example

--- src/PassageControllerInterface.php
+++ src/PassageControllerInterface.php
@@ -5,6 +5,27 @@ namespace Morcen\Passage;
 use Illuminate\Http\Client\Response;
 use Illuminate\Http\Request;
 
+/**
+ * Core contract for Passage handlers.
+ *
+ * Implementations define how inbound requests are transformed before they are
+ * forwarded upstream, how upstream responses are transformed before returning
+ * to the client, and which upstream options should be used for the route.
+ *
+ * In most cases you should extend PassageHandler instead of implementing this
+ * interface directly, since PassageHandler provides no-op defaults for all
+ * three methods and includes the built-in auth and resilience helpers.
+ *
+ * Example:
+ *
+ *   class GithubHandler extends PassageHandler
+ *   {
+ *       public function getOptions(): array
+ *       {
+ *           return ['base_uri' => 'https://api.github.com/'];
+ *       }
+ *   }
+ */
 interface PassageControllerInterface
 {
     /**
